18c893b5b0a3da5f7b50f0f63dfe03ba2fecf42a
Stas <seeyou85@gmail.com>
2018-04-16 00:47:15 +0300

    Adding animation

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -10,6 +10,11 @@ public class GameWindow  extends JFrame {
     private static Image background;
     private static Image gameOver;
     private static Image drop;
+    private static float dropLeft =200;
+    private static float dropTop = 200;
+    private  static long lastFrameTime;
+    private static float dropVelocity = 200;
+
 
     public static void main(String[] args) throws IOException {
         background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
@@ -20,15 +25,23 @@ public class GameWindow  extends JFrame {
         gameWindow.setLocation(200,100);
         gameWindow.setSize(906,478);
         gameWindow.setResizable(false);
+        lastFrameTime = System.nanoTime();
         GameField gameField = new GameField();
         gameWindow.add(gameField);
         gameWindow.setVisible(true);
     }
 
     private static void onRepaint (Graphics g) {
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime-lastFrameTime)*0.000000001f;
+        lastFrameTime = currentTime;
+
         g.drawImage(background,0,0,null);
-        g.drawImage(drop,100,100,null);
-        g.drawImage(gameOver,280,120,null);
+        g.drawImage(drop,(int)dropLeft,(int)dropTop,null);
+
+        dropTop = dropTop +dropVelocity*deltaTime;
+
+        //g.drawImage(gameOver,280,120,null);
     }
 
     private static class GameField extends JPanel {
@@ -36,6 +49,7 @@ public class GameWindow  extends JFrame {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             onRepaint(g);
+            repaint();
         }
     }
 }
